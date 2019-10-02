1e61a619595a481cfc93bcab23f364cf4b107449
Price Shoemaker <priceshoemaker@Prices-MacBook-Pro.local>
2019-10-02 00:35:35 -0500

    Step 3 wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from "./app.component";
 import { MyImageComponent } from "./my-image/my-image.component";
 import { MyButtonComponent } from "./my-button/my-button.component";
 import { MyServiceService } from "./my-service.service";
+import { MessagesComponent } from './models/messages/messages.component';
+import { MessageComponent } from './models/message/message.component';
+import { UserComponent } from './models/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, MyImageComponent, MyButtonComponent],
+  declarations: [AppComponent, MyImageComponent, MyButtonComponent, MessagesComponent, MessageComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [MyServiceService],
   bootstrap: [AppComponent]
